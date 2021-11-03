469f1534af2eec4e439d5232c0ec57b22a97e3e6
WWEMGamer2 <73326807+WWEMGamer2@users.noreply.github.com>
2021-11-03 23:19:02 +1100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,25 @@
+import discord
+import zentools
+import setupfile
+from discord.ext import commands
+from keep_alive import keep_alive
+bot = commands.Bot(command_prefix="COMMAND_PREFIX_HERE")
+
+@bot.event
+async def on_connect():
+  start_setup() # SETUP YOUR BOT
+  print("Starting setup...")
+  
+# COMMANDS:
+
+@bot.command()
+async def test(ctx):
+  print(f"test command executed from {ctx.guild}")
+  await ctx.send("Test")
+  
+# RUN THE BOT:
+
+bot.run("YOUR_TOKEN_HERE")
+
+# PLEASE NOTE: If you are using any public code editor, make sure the token is private. 
+# Anyone can login and destroy your server through the bot.
